6cb3264958424da214c3a1dbad713e8a72e360db
祝融 <211103376@qq.com>
2020-01-04 22:41:26 +0800

    添加Imgui

--- DecodeUE4Log/test.cpp
+++ DecodeUE4Log/test.cpp
@@ -1,46 +1,256 @@
-#include "Decoder/UE4Log.h"
-#include "Decoder/LogFilter.h"
+#pragma comment(lib,"d3d11.lib")
 #include <stdio.h>
 #include <string>
 #include <algorithm>
+#include <d3d11.h>
+#include <dinput.h>
+#include <tchar.h>
+#include "Imgui/imgui.h"
+#include "Imgui/imgui_impl_dx11.h"
+#include "Imgui/imgui_impl_win32.h"
+#include "Decoder/UE4Log.h"
+#include "Decoder/LogFilter.h"
 
-int main()
+// Data
+static ID3D11Device*            g_pd3dDevice = NULL;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
+static IDXGISwapChain*          g_pSwapChain = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	CUELogFile Log;
-	char Path[256];
-	scanf_s("%s", Path, 256);
-	if (Path[0] == '"')
-	{
-		Path[strlen(Path) - 1] = 0;
-	}
-	Log.LoadLogFile(Path);
+	// Create application window
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
+	::RegisterClassEx(&wc);
+	HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("Imgui Simple"), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
 
-	printf("共解析%d条Log\n", Log.GetLogCount());
-
-	vector<string> AllNames = Log.GetLogTypeNames();
-
-	for (auto val : AllNames)
+	// Initialize Direct3D
+	if (!CreateDeviceD3D(hwnd))
 	{
-		printf("%s\n", val.c_str());
-	}
-	
-	CUELogFilter Filter(&Log);
-	Filter.CloseAllLogLevelFilter();
-	Filter.CloseAllLogTypeFilter();
-	Filter.OpenLogLevelFilter(ELogLevel::Error);
-	Filter.OpenLogTypeFilter(Log.GetLogTypeCode("LogResMgr"));
-	Filter.Filtrate();
-	
-	decltype(auto) FiltResult = Filter.GetFiltResult();
-	CUELogSorter sorter(FiltResult);
-	sorter.SortByTime_Asce();
-	for (auto val : FiltResult)
-	{
-		printf("%s  %s  %s\n", val->GetLogTime().GetTimeStr().ToString().c_str(), val->GetLogTypeName().ToString().c_str(), val->GetLogContent().ToString().c_str());
+		CleanupDeviceD3D();
+		::UnregisterClass(wc.lpszClassName, wc.hInstance);
+		return 1;
 	}
 
+	// Show the window
+	::ShowWindow(hwnd, SW_SHOWDEFAULT);
+	::UpdateWindow(hwnd);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGuiStyle& style = ImGui::GetStyle();
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	io.IniFilename = nullptr;
+	style.WindowRounding = 0;
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsLight();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplWin32_Init(hwnd);
+	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+	// add Chinese font
+	io.Fonts->AddFontFromFileTTF("c:/windows/fonts/simhei.ttf", 15, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+
+	// Our state
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	// Main loop
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT)
+	{
+		if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+			continue;
+		}
+
+		// Start the Dear ImGui frame
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			ImGui::BeginMainMenuBar();
+			if (ImGui::BeginMenu("File"))
+			{
 
 
-	system("Pause");
+				ImGui::EndMenu();
+			}
+			ImGui::EndMainMenuBar();
+
+			ImGui::Begin("mainWindow", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_HorizontalScrollbar);
+
+			ImGui::SetWindowPos({ 0,20 });
+			ImGui::SetWindowSize({ io.DisplaySize.x,io.DisplaySize.y - 20 });
+
+			ImGui::Text("                                           This is some useful text.");               // Display some text (you can use a format strings too)
+
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+
+		}
+
+
+		// Rendering
+		ImGui::Render();
+		g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+		g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+		g_pSwapChain->Present(1, 0);
+	}
+
+	// Cleanup
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	CleanupDeviceD3D();
+	::DestroyWindow(hwnd);
+	::UnregisterClass(wc.lpszClassName, wc.hInstance);
+
 	return 0;
 }
+
+// Helper functions
+bool CreateDeviceD3D(HWND hWnd)
+{
+	// Setup swap chain
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 2;
+	sd.BufferDesc.Width = 0;
+	sd.BufferDesc.Height = 0;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	UINT createDeviceFlags = 0;
+	//createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+	D3D_FEATURE_LEVEL featureLevel;
+	const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+	if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+		return false;
+
+	CreateRenderTarget();
+	return true;
+}
+
+void CleanupDeviceD3D()
+{
+	CleanupRenderTarget();
+	if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
+	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+}
+
+void CreateRenderTarget()
+{
+	ID3D11Texture2D* pBackBuffer;
+	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+	pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+}
+
+// Win32 message handler
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+
+	switch (msg)
+	{
+	case WM_SIZE:
+		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+		{
+			CleanupRenderTarget();
+			g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+			CreateRenderTarget();
+		}
+		return 0;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+			return 0;
+		break;
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		return 0;
+	}
+	return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
+
+// int main()
+// {
+// 	CUELogFile Log;
+// 	char Path[256];
+// 	scanf_s("%s", Path, 256);
+// 	if (Path[0] == '"')
+// 	{
+// 		Path[strlen(Path) - 1] = 0;
+// 	}
+// 	Log.LoadLogFile(Path);
+// 
+// 	printf("共解析%d条Log\n", Log.GetLogCount());
+// 
+// 	vector<string> AllNames = Log.GetLogTypeNames();
+// 
+// 	for (auto val : AllNames)
+// 	{
+// 		printf("%s\n", val.c_str());
+// 	}
+// 	
+// 	CUELogFilter Filter(&Log);
+// 	Filter.CloseAllLogLevelFilter();
+// 	Filter.CloseAllLogTypeFilter();
+// 	Filter.OpenLogLevelFilter(ELogLevel::Error);
+// 	Filter.OpenLogTypeFilter(Log.GetLogTypeCode("LogResMgr"));
+// 	Filter.Filtrate();
+// 	
+// 	decltype(auto) FiltResult = Filter.GetFiltResult();
+// 	CUELogSorter sorter(FiltResult);
+// 	sorter.SortByTime_Asce();
+// 	for (auto val : FiltResult)
+// 	{
+// 		printf("%s  %s  %s\n", val->GetLogTime().GetTimeStr().ToString().c_str(), val->GetLogTypeName().ToString().c_str(), val->GetLogContent().ToString().c_str());
+// 	}
+// 
+// 
+// 
+// 	system("Pause");
+// 	return 0;
+// }
